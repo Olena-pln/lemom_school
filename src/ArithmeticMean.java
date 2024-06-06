88d81a81ce8c6f2f1c9f77f0a3d4798ff5e80702
Olena <olenavygovska2019@gmail.com>
2024-06-06 16:31:47 +0200

    Fix arithmetic mean

--- src/ArithmeticMean.java
+++ src/ArithmeticMean.java
@@ -6,14 +6,12 @@ public class ArithmeticMean {
         Scanner reader = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
-        int a = reader.nextInt();
+        float a = reader.nextInt();
         System.out.print("Enter second number: ");
-        int b = reader.nextInt();
+        float b = reader.nextInt();
         System.out.print("Enter third number: ");
-        int c = reader.nextInt();
+        float c = reader.nextInt();
 
-        int mean;
-        mean = (a + b + c)/3;
-        System.out.println("Arithmetic mean: " + mean);
+        System.out.println("Arithmetic mean: " + (a + b + c)/3);
         }
     }
