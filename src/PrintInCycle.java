c0661dd3518245b95513437f9bb5a15b75cdb801
Olena <olenavygovska2019@gmail.com>
2024-06-06 15:48:24 +0200

    Add print in cycle

---
+++ src/PrintInCycle.java
@@ -0,0 +1,8 @@
+public class PrintInCycle {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            String s = String.valueOf(i);
+            System.out.println("Test" + s);
+        }
+    }
+}
